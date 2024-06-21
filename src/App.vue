2bf9143407092def40187679a6f88d9dc6247db5
marifahmad <marifahmad145@gmail.com>
2024-06-21 14:33:19 +0700

    mambuat tombol tugas 1 sampai 7

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <button @click="showModal = true; isEditMode = false;" class="add-button">Tambah Data</button>
+    <button @click="showTasks = !showTasks" class="tugas" style="margin-left: 5px; background-color: #07a30c; color: #fff;">Tugas 1 - 7</button>
     <h2 style="text-align: start;">Data Pariwisata</h2>
     <div v-if="showModal" class="modal" style="margin-top: 20px;">
       <div class="modal-content">
@@ -15,14 +16,13 @@
 
          
           <div class="radio-label" style="margin-bottom: 10px;">
-  <label>
-    
-    <input type="radio" v-model="form.gender" value="Laki-laki" /> Laki-laki
-  </label>
-  <label>
-    <input type="radio" v-model="form.gender" value="Perempuan" /> Perempuan
-  </label>
-</div>
+            <label>
+              <input type="radio" v-model="form.gender" value="Laki-laki" /> Laki-laki
+            </label>
+            <label>
+              <input type="radio" v-model="form.gender" value="Perempuan" /> Perempuan
+            </label>
+          </div>
 
           <input type="text" v-model="form.turis" placeholder="Negara Asal" required />
           <input type="text" v-model="form.durasi" placeholder="Durasi Kunjungan" required />
@@ -47,23 +47,42 @@
       <tbody>
         <tr v-for="(article, index) in articles" :key="article.id">
           <td>{{ index + 1 }}</td>
-          <td > {{ article.tanggal }}</td>
+          <td>{{ article.tanggal }}</td>
           <td>{{ article.wisatawan }}</td>
           <td>{{ article.usia }}</td>
           <td>{{ article.gender }}</td>
           <td>{{ article.turis }}</td>
           <td>{{ article.durasi }}</td>
-          
-
-          
-          <td style="">
-            <button class="edit-button" style=" background-color: rgb(11, 182, 240); font-size: 15px; width: 50px; padding: none; margin-right: 10px; margin-bottom: 5px;" @click="editArticle(article)"><i class="fas fa-edit edit-button" style="background-color: rgb(11, 182, 240)"></i></button>
-            <button class="delete-button" style=" background-color: #d32f2f; margin-top: 10px; font-size: 15px; width: 50px; padding: none; margin-bottom: 5px;" @click="deleteArticle(article.id)"><i class="fas fa-trash delete-button" style="background-color: #d32f2f;" ></i></button>
+          <td>
+            <button class="edit-button" style="background-color: rgb(11, 182, 240); font-size: 15px; width: 50px; padding: none; margin-right: 10px; margin-bottom: 5px;" @click="editArticle(article)"><i class="fas fa-edit edit-button" style="background-color: rgb(11, 182, 240)"></i></button>
+            <button class="delete-button" style="background-color: #d32f2f; margin-top: 10px; font-size: 15px; width: 50px; padding: none; margin-bottom: 5px;" @click="deleteArticle(article.id)"><i class="fas fa-trash delete-button" style="background-color: #d32f2f;"></i></button>
           </td>
         </tr>
       </tbody>
     </table>
+    <!-- List tugas -->
   </div>
+  <div v-if="showTasks" class="task-list" style="margin-top: 5px;">
+    <h3 style="padding-top: 10px;">List Tugas:</h3>
+    <div class="listnya" style="text-align: center; display: flex; align-items: center; justify-content: center;">
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+          <a :href="task.url" target="_blank" @click="showAlert(task)">{{ task.title }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- modalalert -->
+  <div v-if="showCustomAlert" class="custom-alert">
+      <div class="custom-alert-content">
+        <span class="custom-alert-close" @click="showCustomAlert = false">&times;</span>
+        <h2>Alert</h2>
+        <p>Apakah Anda Mau Tugas 2?</p>
+        <button @click="navigateToTask2" class="alert-button">Ya</button>
+        <button @click="showCustomAlert = false" class="alert-button">Tidak</button>
+      </div>
+    </div>
+    
   <footer>
     <p>&copy; 2024 Integrasi Dengan API. All rights reserved.</p>
     <p>Contact: marifahmad145@gmail.com</p>
@@ -80,6 +99,18 @@ export default {
     FontAwesomeIcon
   },
   setup() {
+    
+    // Data tugas
+    const tasks = reactive([
+      { title: 'Tugas 1', url: 'https://ahmad-projectcv.netlify.app' },
+      { title: 'Tugas 2', url: 'https://FreeTugas' },
+      { title: 'Tugas 3', url: 'https://marifahmad.netlify.app/' },
+      { title: 'Tugas 4', url: 'https://marifahmad-223510434-prapbk.netlify.app' },
+      { title: 'Tugas 5', url: 'https://marifahmad-223510434-ppbk-t5-landingp.netlify.app' },
+      { title: 'Tugas 6', url: 'https://marifahmadnasution-223510434-t6-inapi.netlify.app' },
+      { title: 'Tugas 7', url: 'https://marifahmadnasution-223510434-t7-pinia.netlify.app' }
+    ]);
+    const showTasks = ref(false);
     const articles = ref([]);
     const binId = '665a5652acd3cb34a850d96b'; // Replace with your actual bin ID from JSON Bin
     const apiKey = '$2a$10$3h0EzbkfDqg5XVaa/5AIW.PX9kFIUSnu54dNCGtFLY4ZO6l2Sulq.'; // Replace with your actual API key from JSON Bin
@@ -95,7 +126,7 @@ export default {
       usia: '',
       gender: '',
       turis: '',
-      durasi:''
+      durasi: ''
     });
 
     const fetchData = async () => {
@@ -167,60 +198,65 @@ export default {
       form.usia = article.usia;
       form.gender = article.gender;
       form.turis = article.turis;
-      form.durasi =article.durasi;
+      form.durasi = article.durasi;
       showModal.value = true;
     };
 
     const onSubmit = async () => {
-      try {
-        const newData = {
-          tanggal: form.tanggal,
-          wisatawan: form.wisatawan,
-          usia: form.usia,
-          gender: form.gender,
-          turis: form.turis,
-          durasi: form.durasi,
-          id: isEditMode.value ? currentArticleId.value : new Date().toISOString() // Generating unique ID for new articles
-        };
+      const articleData = {
+        id: isEditMode.value ? currentArticleId.value : Date.now(),
+        tanggal: form.tanggal,
+        wisatawan: form.wisatawan,
+        usia: form.usia,
+        gender: form.gender,
+        turis: form.turis,
+        durasi: form.durasi
+      };
 
+      try {
         if (isEditMode.value) {
-          await updateData(currentArticleId.value, newData);
+          await updateData(currentArticleId.value, articleData);
         } else {
-          await addData(newData);
+          await addData(articleData);
         }
-        form.tanggal = '';
-        form.wisatawan = '';
-        form.usia = '';
-        form.gender = '';
-        form.turis = '';
-        form.durasi = '';
-        
-       
         showModal.value = false;
-        isEditMode.value = false;
-        currentArticleId.value = null;
+        resetForm();
         await fetchData();
       } catch (error) {
-        console.error('Error adding/updating data:', error);
+        console.error('Error submitting form:', error);
+      }
+    };
+    
+
+    const resetForm = () => {
+      form.tanggal = '';
+      form.wisatawan = '';
+      form.usia = '';
+      form.gender = '';
+      form.turis = '';
+      form.durasi = '';
+      isEditMode.value = false;
+    };
+
+    const showAlert = (task) => {
+      if (task.title === 'Tugas 2') {
+        alert('Apakah Anda Mau Tugas 2?');
       }
     };
 
-    onMounted(async () => {
-      await fetchData();
-    });
+    onMounted(fetchData);
 
     return {
-      articles,
       showModal,
-      isEditMode, 
-      currentArticleId,
+      isEditMode,
       form,
-      fetchData,
-      addData,
-      updateData,
-      deleteArticle,
+      articles,
+      onSubmit,
       editArticle,
-      onSubmit
+      deleteArticle,
+      showTasks,
+      tasks,
+      showAlert
     };
   }
 };
@@ -252,7 +288,7 @@ body {
 .add-button {
   padding: 10px 20px;
   margin-bottom: 20px;
-  background-color: #4caf50;
+  background-color: #07a30c;
   color: white;
   border: none;
   border-radius: 5px;
@@ -345,7 +381,53 @@ textarea {
   
 }
 
+.task-list {
+  margin-top: 10px;
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(10px);
+  border-radius: 10px;
+  padding: 20px 30px;
+  box-shadow: 0 8px 32px rgba(0, 0, 0, 0.37);
+  color: #fff;
+  text-align: center;
+  width: 100%;
+  max-width: 1400px;
+  
+}
 
+.task-list ul {
+  list-style-type: none;
+ 
+}
+
+.task-list li {
+  margin-bottom: 10px;
+  border: 1px solid #4caf50;
+  width: 200px;
+  padding: 10px 20px;
+  background-color: #4caf50;
+  transition: transform 0.3s ease, box-shadow 0.3s ease; /* Efek transisi untuk animasi zoom dan bayangan */
+  display: block; /* Menjadikan item tugas menempati satu baris */
+  border-radius: 5px; /* Agar pinggiran elemen tugas lebih halus */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Bayangan standar */
+}
+
+.task-list li:hover {
+  transform: scale(1.1); /* Memperbesar elemen saat dihover */
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2); /* Bayangan lebih intens saat dihover */
+}
+
+.task-list a {
+  text-align: center;
+  color: #fff;
+  text-decoration: none;
+  transition: color 0.3s ease;
+  display: block; /* Agar teks dalam tautan ditampilkan dalam satu baris */
+}
+
+.task-list a:hover {
+  color: #08440a;
+}
 
 
 
@@ -362,7 +444,7 @@ footer {
   text-align: center;
   border-radius: 10px;
   width: 100%;
-  max-width: 1500px;
+  max-width: 1400px;
 }
 
 footer p {
